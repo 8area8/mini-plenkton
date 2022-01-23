601925522522cfe4ba11a6b73edb1a8eb88f323c
Mikael <mbriolet.ma@gmail.com>
2022-01-23 20:34:58 +0100

    Add tailwind to nuxt

---
+++ App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline m-6">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import './assets/css/tailwind.css'
+</script>
