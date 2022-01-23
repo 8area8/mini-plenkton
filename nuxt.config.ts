4053bf1ad85bbf6e2db19adde43f84ebba1f2996
Mikael <mbriolet.ma@gmail.com>
2022-01-23 22:39:41 +0100

    Add lambda preset for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'lambda'
+  },
   css: ["@/assets/css/fonts.css"],
   build: {
     postcss: {
