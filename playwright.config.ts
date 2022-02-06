dda05c612d461b458253f0ec978978af685881d7
Mikael Briolet <mbriolet.ma@gmail.com>
2022-02-06 11:40:36 +0100

    19 add playwright (#20)
    * add typescript jest
    
    * Add e2e tests

---
+++ playwright.config.ts
@@ -0,0 +1,62 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+// fix ts error: https://github.com/microsoft/playwright/issues/7121#issuecomment-878795543
+import appModulePath from "app-module-path"
+
+appModulePath.addPath(__dirname)
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+
+  testDir: './tests/e2e',
+
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+
+  expect: {
+    timeout: 5000
+  },
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+
+  reporter: 'html',
+
+  use: {
+    actionTimeout: 0,
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: {
+    //     ...devices['Pixel 5'],
+    //   },
+    // },
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Google Chrome',
+    //   use: {
+    //     channel: 'chrome',
+    //   },
+    // },
+  ],
+  outputDir: 'test-results/',
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: process.env.CI ? 'npm run start' : undefined,
+    port: 3000,
+  },
+};
+export default config;
