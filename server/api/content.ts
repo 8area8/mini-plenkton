82ad2c5b423bdb675ba4330bc67f8a371ace114c
Mikael Briolet <mbriolet.ma@gmail.com>
2022-02-09 09:38:47 +0100

    content update - order by date, reversed

--- server/api/content.ts
+++ server/api/content.ts
@@ -5,12 +5,12 @@ import { marked } from 'marked'
 import matter from 'gray-matter'
 
 interface IFileContent {
-    meta: object;
+    meta: any;
     content: string;
 }
 
 /**
- * Get the markdown files.
+ * Get the markdown files ordered by dates.
  * 
  * @param start base starting file (default start after the first file)
  * @param max max files to get
@@ -18,7 +18,7 @@ interface IFileContent {
 export const getContent = (start: number = 1, max: number = 4): IFileContent[] => {
     console.log(process.cwd())
     const directoryPath = process.cwd() + '/content'
-    const filenames = readdirSync(directoryPath).slice(start - 1, start - 1 + max);
+    const filenames = readdirSync(directoryPath);
 
     // /!\ avoid compatibility errors between jest-ts and nuxt 3 nitro
     let read: any
@@ -29,8 +29,19 @@ export const getContent = (start: number = 1, max: number = 4): IFileContent[] =
         read = matter.read
     }
 
-    const filesContent = filenames.map(filename => read(`${directoryPath}/${filename}`))
-    return filesContent.map(file => {
+    const files = filenames.map(filename => read(`${directoryPath}/${filename}`))
+    const sortedfiles = files.sort((a, b) => {
+        const aDate = new Date(a.data.added)
+        const bDate = new Date(b.data.added)
+        if (aDate < bDate) {
+            return -1
+        } else if (aDate > bDate) {
+            return 1
+        }
+        return 0
+    }).reverse()
+    const slicedfiles = sortedfiles.slice(start - 1, start - 1 + max)
+    return slicedfiles.map(file => {
         return { meta: file.data, content: marked(file.content) }
     })
 }
--- tests/content.test.ts
+++ tests/content.test.ts
@@ -1,70 +1,106 @@
-import { getContent } from "../server/api/content"
-import { mocked } from 'jest-mock'
+import { getContent } from "../server/api/content";
+import { mocked } from "jest-mock";
 
-jest.mock('fs');
-import * as fs from 'fs'
-const mockFs = mocked(fs)
+jest.mock("fs");
+import * as fs from "fs";
+const mockFs = mocked(fs);
 
-
-test('Content is marked', () => {
-    const TestData: string = 'content1';
-    mockFs.readdirSync.mockReturnValue(["file1"] as any)
+test("Content is marked", () => {
+    const TestData: string = "content1";
+    mockFs.readdirSync.mockReturnValue(["file1"] as any);
     mockFs.readFileSync.mockReturnValue(TestData);
 
-    const files = getContent()
-    const file = files[0]
-    expect(file.content).toBe("<p>content1</p>\n")
+    const files = getContent();
+    const file = files[0];
+    expect(file.content).toBe("<p>content1</p>\n");
 });
 
-test('Content has meta', () => {
-    const TestData: string = '---\ntitle: testTitle\n---\ncontent1';
-    mockFs.readdirSync.mockReturnValue(["file1"] as any)
+test("Content has meta", () => {
+    const TestData: string = "---\ntitle: testTitle\n---\ncontent1";
+    mockFs.readdirSync.mockReturnValue(["file1"] as any);
     mockFs.readFileSync.mockReturnValue(TestData);
 
-    const files = getContent()
-    const file = files[0]
-    expect(file.meta).toEqual({ title: "testTitle" })
+    const files = getContent();
+    const file = files[0];
+    expect(file.meta).toEqual({ title: "testTitle" });
 });
 
-test('Content gets 1 file', () => {
-    const TestData: string = 'content1';
-    mockFs.readdirSync.mockReturnValue(["file1"] as any)
+test("Content gets 1 file", () => {
+    const TestData: string = "content1";
+    mockFs.readdirSync.mockReturnValue(["file1"] as any);
     mockFs.readFileSync.mockReturnValue(TestData);
 
-    const files = getContent()
-    expect(files).toHaveLength(1)
-    const file = files[0]
-    expect(file.content).toBe("<p>content1</p>\n")
+    const files = getContent();
+    expect(files).toHaveLength(1);
+    const file = files[0];
+    expect(file.content).toBe("<p>content1</p>\n");
 });
 
-test('Content when we start at 2', () => {
-    const testData: string[] = ['content2', 'content3', 'content4', 'content5'];
-    mockFs.readdirSync.mockReturnValue(["1", '2', '3', '4', '5', '6'] as any)
+test("Content when we start at 2", () => {
+    const testData: string[] = [
+        "---\nadded: 06/01/01\n---\ncontent1",
+        "---\nadded: 05/01/01\n---\ncontent2",
+        "---\nadded: 04/01/01\n---\ncontent3",
+        "---\nadded: 03/01/01\n---\ncontent4",
+        "---\nadded: 02/01/01\n---\ncontent5",
+        "---\nadded: 01/01/01\n---\ncontent6",
+    ];
+    mockFs.readdirSync.mockReturnValue(["1", "2", "3", "4", "5", "6"] as any);
     for (const data of testData) {
-        mockFs.readFileSync.mockReturnValueOnce(data)
+        mockFs.readFileSync.mockReturnValueOnce(data);
     }
 
-    const files = getContent(2)
-    expect(files).toHaveLength(4)
-    let index = 2
+    const files = getContent(2);
+    expect(files).toHaveLength(4);
+    let index = 2;
     for (const file of files) {
-        expect(file.content).toBe(`<p>content${index}</p>\n`)
-        index++
+        expect(file.content).toBe(`<p>content${index}</p>\n`);
+        index++;
     }
 });
 
-test('Content when we start at 2, ends at 3', () => {
-    const testData: string[] = ['content2', 'content3', 'content4'];
-    mockFs.readdirSync.mockReturnValue(["1", '2', '3', '4', '5', '6'] as any)
+test("Content when we start at 2, ends at 3", () => {
+    const testData: string[] = [
+        "---\nadded: 06/01/01\n---\ncontent1",
+        "---\nadded: 05/01/01\n---\ncontent2",
+        "---\nadded: 04/01/01\n---\ncontent3",
+        "---\nadded: 03/01/01\n---\ncontent4",
+        "---\nadded: 02/01/01\n---\ncontent5",
+        "---\nadded: 01/01/01\n---\ncontent6",
+    ];
+    mockFs.readdirSync.mockReturnValue(["1", "2", "3", "4", "5", "6"] as any);
     for (const data of testData) {
-        mockFs.readFileSync.mockReturnValueOnce(data)
+        mockFs.readFileSync.mockReturnValueOnce(data);
     }
 
-    const files = getContent(2, 3)
-    expect(files).toHaveLength(3)
-    let index = 2
+    const files = getContent(2, 3);
+    expect(files).toHaveLength(3);
+    let index = 2;
     for (const file of files) {
-        expect(file.content).toBe(`<p>content${index}</p>\n`)
-        index++
+        expect(file.content).toBe(`<p>content${index}</p>\n`);
+        index++;
+    }
+});
+
+test("Content returns documents ordered by date, reversed", () => {
+    const testData: string[] = [
+        "---\nadded: 01/01/01\n---\ncontent1",
+        "---\nadded: 04/01/01\n---\ncontent2",
+        "---\nadded: 06/01/01\n---\ncontent3",
+        "---\nadded: 02/01/01\n---\ncontent4",
+        "---\nadded: 03/01/01\n---\ncontent5",
+        "---\nadded: 05/01/01\n---\ncontent6",
+    ];
+    mockFs.readdirSync.mockReturnValue(["1", "2", "3", "4", "5", "6"] as any);
+    for (const data of testData) {
+        mockFs.readFileSync.mockReturnValueOnce(data);
+    }
+
+    const files = getContent();
+    const dates = ["06/01/01", "05/01/01", "04/01/01", "03/01/01"]
+    let index = 0
+    for (const file of files) {
+        expect(file.meta.added).toBe(dates[index]);
+        index++;
     }
 });
